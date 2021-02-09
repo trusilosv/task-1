6cba2f9631a12f873b05e8f6e37c64ea31a7bc24
Your Name <you@example.com>
2021-02-09 09:46:50 +0300

    feat: add ivent close

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let arrcards = document.querySelectorAll('.card');
 arrcards.forEach((el) => {
-    el.style.backgroundColor = "#" + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase()
+    el.style.backgroundColor = "#" + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+    el.children[0].addEventListener('click', (cl) => {
+        cl.target.parentNode.remove();
+    })
 });
