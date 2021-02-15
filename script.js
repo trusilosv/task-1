e71a6bd4496f790ac519c75ce1877fd065a9f282
Your Name <you@example.com>
2021-02-15 15:43:31 +0300

    feat: add grid

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 let arrcards = document.querySelectorAll('.card');
+
 arrcards.forEach((el) => {
-    el.style.backgroundColor = "#" + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+    el.style.backgroundColor = `#${(Math.random().toString(16) + '000000').substring(2, 8).toUpperCase()}`;
     el.children[0].addEventListener('click', (cl) => {
         cl.target.parentNode.remove();
     })
 });
+const CardField = document.getElementById('cardfield');
+const InputSmile = document.getElementById('checker');
+const LableSmile = document.getElementById('labelSmile');
+LableSmile.addEventListener('click', () => {
+    if (InputSmile.checked) CardField.classList.add('grid')
+    else CardField.classList.remove('grid');
+});
+
+
+
+
+// function rot(strng) {
+//     return strng.split('').reverse().join('')
+// }
+
+// function selfieAndRot(strng) {
+//     const dots = '.'.repeat(strng.split('\n')[0].length)
+//     return strng.split('\n').join(dots + '\n') + dots + '\n' + dots + rot(strng).split('\n').join('\n' + dots)
+// }
+
+// function oper(fct, s) {
+//     return fct(s);
+// }
+// alert(oper(selfieAndRot, 'rkKv\ncofM\nzXkh\nflCB'));
